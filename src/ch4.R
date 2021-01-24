4a9fc435b838118507139a49f28b5798b7d8e81a
cypowers <devin17@knou.ac.kr>
2021-01-24 14:00:03 +0900

    4.9.1.2 추가

--- src/ch4.R
+++ src/ch4.R
@@ -15,3 +15,42 @@ scatter <- ggplot(examData, aes(Anxiety, Exam, colour = Gender))
 scatter + geom_point() + geom_smooth(method = 'lm')
 scatter + geom_point() + geom_smooth(method = 'lm', aes(fill = Gender), alpha = 0.1)
 scatter + geom_point() + geom_smooth(method = 'lm', aes(fill = Gender), alpha = 0.1) + labs(x = 'Exam Anxiety', y = 'Exam Performance %', colour = 'Gender')
+
+# histogram
+festivalData <- read.delim("data/DownloadFestival.dat", header = TRUE)
+festivalHistogram <- ggplot(festivalData, aes(day1)) + theme(legend.position = "none")
+festivalHistogram + geom_histogram()
+festivalHistogram + geom_histogram( binwidth = 0.4) + labs(x = "Hygiene (Day 1 of Festival)", y = "Frequency")
+
+festivalBoxplot <- ggplot(festivalData, aes(gender, day1))
+festivalBoxplot + geom_boxplot() + labs(x = "Gender", y = "Hygiene (Day 1 of Festival)")
+tail(festivalData[order(festivalData$day1),])
+
+# 이상치 변경
+festivalData$day1[festivalData$ticknumb == 4158] <- 2.02
+
+festivalBoxplot <- ggplot(festivalData, aes(gender, day2))
+festivalBoxplot + geom_boxplot() + labs(x = "Gender", y = "Hygiene (Day 1 of Festival)")
+
+festivalBoxplot <- ggplot(festivalData, aes(gender, day3))
+festivalBoxplot + geom_boxplot() + labs(x = "Gender", y = "Hygiene (Day 1 of Festival)")
+
+density <- ggplot(festivalData, aes(day1))
+density + geom_density()
+density + geom_density() + labs(x = "Hygiene (Day 1 of Festival)", y = "Density Estimate")
+
+# 4.9.1.1
+chickFlick <- read.delim("data/ChickFlick.dat", header = TRUE)
+bar <- ggplot(chickFlick, aes(film, arousal))
+bar + stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black")
+bar + stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black") + stat_summary(fun.data = mean_cl_normal, geom = "pointrange")
+bar + stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black") + stat_summary(fun.data = mean_cl_normal, geom = "pointrange")
+bar + stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black") + stat_summary(fun.data = mean_cl_normal, geom = "pointrange") + labs( x = "Film", y = "Mean Arousal")
+
+# 4.9.1.2
+bar <- ggplot(chickFlick, aes(film, arousal, fill = gender))
+bar + stat_summary(fun = mean, geom = "bar", position="dodge")
+bar + stat_summary(fun = mean, geom = "bar", position="dodge") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", position = position_dodge(width=0.90), width=0.2) + labs( x = "Film", y = "Mean Arousal", fill = "Gender")
+
+bar <- ggplot(chickFlick, aes(film, arousal, fill = film))
+bar + stat_summary(fun = mean, geom = "bar") + stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width=0.2) + facet_wrap(~ gender) + labs( x = "Film", y = "Mean Arousal") + theme(legend.position = "")
